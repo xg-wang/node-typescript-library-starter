b384230074cb1de89565559db3191193f50ae00a
Thomas Wang <hi@xgwang.me>
2021-09-16 08:52:22 -0700

    Jest ESM setup

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,10 +1,12 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: "ts-jest",
+  preset: 'ts-jest/presets/default-esm',
   globals: {
     "ts-jest": {
+      useESM: true,
       tsconfig: "test/tsconfig.json",
     },
   },
+  // transformIgnorePatterns: ["node_modules/(?!(node-fetch))"],
   testEnvironment: "node",
 };
